a73804c33e591389a9824c46f5be15393db7e87f
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-05-28 09:27:51 +0530

    added server nodemon adn little

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", (req, res) => {
+res.send("gellp worlkd")
+})
+
 app.listen(3000, ()=>{
     console.log("server is runnign at 3000");
 })
