aa034d6f22aa107f59be9a9180773b0b4ac06096
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-05-28 14:04:44 +0530

    added log in fuctionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -57,9 +57,32 @@ export const signup = async (req, res) => {
 }
 
 
-export const login = (req, res) => {
-    console.log("loginuser");
-}
+export const login = async (req, res) => {
+
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if(!user || !isPasswordCorrect){
+            return res.status(400).json({error: "inavlid login credentials"});
+
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+        res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        username: user.username,
+        profilePic: user.profilePic,
+        });
+
+    } catch(error){
+        console.log("error in login controller ", error.message);
+        res.status(500).json({error: "internal server error"});
+    }
+    
+};
 
 export const logout = (req, res) => {
     res.send("bye bye")
