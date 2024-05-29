a6c5ede5820abbae546f88a169d44d9be54cb45c
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-05-29 17:41:58 +0530

    added frontedn of signup and login

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
 
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
+  return <div className='flex flex-col item-center justify-center min-w-96 mx-auto'>
+    <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg
+    bg-opacity-0'>  
+    {/*  tai
+    lwind css glassmorphism*/}
+        <h1 className='text-3xl font-semibold text-center text-gray-300'>
+            Login
+            <span className='text-blue-500'>ChitChat</span>
+            </h1>
+
+            <form>
+                <div>
+                    <label className='label p-2'>
+                        <span className='text-base label-text'>Username</span>
+                    </label>
+                    <input type="text" placeholder = "Enter Username " className='w-full input input-bordered h-10' />
+                </div>
+
+
+                <div>
+                    <label className="label">
+                        <span className='text-base label-text'>Password</span>
+                    </label>
+                    <input type="text"
+                    placeholder='Enter Password'
+                    className='w-full input input-bordered h-10' />
+                </div>
+
+                <a href="#" className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block'>
+                    {"Don't"} have an account?
+                </a>
+
+                <div>
+                    <button className='btn btn-block btn-sm mt-2'>Log In</button>
+                </div>
+            </form>
+    </div>
+    
+    </div>;
 }
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,7 +1,81 @@
 import React from 'react'
-
+import { GenderCheckbox } from './GenderCheckbox.jsx'
 export const SignUp = () => {
-  return (
-    <div>SignUp</div>
-  )
+  return <div  className='flex flex-col item-center justify-center min-w-96 mx-auto'>
+
+<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg
+    bg-opacity-0'>  
+
+    <h1 className='text-3xl font-semibold text-center text-gray-300'>
+        Sign Up <span className='text-blue-500'>ChitChat</span>
+    </h1>
+
+    <form >
+
+    <div>
+		<label className='label p-2'>
+			<span className='text-base label-text'>Full Name</span>
+			</label>
+			<input
+				type='text'
+				placeholder='Navdeep Singh'
+				className='w-full input input-bordered  h-10'
+				// value={inputs.fullName}
+				// onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+			/>
+	</div>
+
+    <div>
+		<label className='label p-2'>
+			<span className='text-base label-text'>Username</span>
+			</label>
+			<input
+				type='text'
+				placeholder='navdeep'
+				className='w-full input input-bordered  h-10'
+				// value={inputs.fullName}
+				// onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+			/>
+	</div>
+
+    <div>
+		<label className='label'>
+			<span className='text-base label-text'>Password</span>
+			</label>
+			<input
+				type='password'
+				placeholder='Enter Password'
+				className='w-full input input-bordered  h-10'
+				// value={inputs.fullName}
+				// onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+			/>
+	</div>
+
+    <div>
+		<label className='label'>
+			<span className='text-base label-text'>Confirm Password</span>
+			</label>
+			<input
+				type='password'
+				placeholder='Confirm Password'
+				className='w-full input input-bordered  h-10'
+				// value={inputs.fullName}
+				// onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+			/>
+	</div>
+
+    <GenderCheckbox />
+
+    <a className='text-sm hover:underline hover-text-blue-600 mt-2 inline-block' 
+    href="#">
+        Already have an account? Login
+    </a>
+
+    <div>
+        <button className='btn btn-block btn-sm mt-2'>Sign Up</button>
+    </div>
+    </form>
+    </div>
+
+  </div>
 }
