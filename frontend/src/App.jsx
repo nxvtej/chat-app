16bbb06f57175697ad9d873c3af36894bbe04327
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-05-29 19:00:31 +0530

    react icon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import './App.css'
 import './index.css'
 import { Login } from './pages/login/Login.jsx';
 import { SignUp } from './pages/signup/SignUp.jsx';
-
+import { Home } from './pages/home/Home.jsx';
 
 function App() {
 
   return <div className='p-4 h-screen flex items-center justify-center'>
       {/* <Login /> */}
-      <SignUp/>
+      {/* <SignUp/> */}
+      <Home />
 
       
     </div>;
---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import  SearchInput  from './SearchInput.jsx';
+
+const Sidebar = () => {
+  return (
+    <div>
+        <SearchInput />
+        {/* <div className='divider px-3'></div> */}
+        {/* <Conversation />
+        <LogoutButton /> */}
+    </div>
+  )
+}
+
+export default Sidebar
