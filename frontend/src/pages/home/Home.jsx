120460d6403fecc9ba70e30699a8f20a34af2dc7
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-05-30 16:45:37 +0530

    design completed

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import MessageContainer from '../../components/messages/MessageContainer.jsx';
 export const Home = () => {
   return (
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-			{/* <Sidebar /> */}
+			<Sidebar />
 			<MessageContainer />
 		</div>
 	);
