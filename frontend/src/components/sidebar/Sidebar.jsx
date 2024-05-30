b9517f5c27328b64eb0eeb0681b131522cfa8303
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-05-30 15:29:53 +0530

    added conversations

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import  SearchInput  from './SearchInput.jsx';
+import Conversations from './Conversations.jsx';
 
 const Sidebar = () => {
   return (
-    <div>
+    <div className='border-r border-slate-500 p-4 flex flex-col'>
         <SearchInput />
+        <Conversations />
         {/* <div className='divider px-3'></div> */}
-        {/* <Conversation />
-        <LogoutButton /> */}
+        {/* <Conversation /> */}
+        {/* <LogoutButton /> */}
     </div>
   )
 }
