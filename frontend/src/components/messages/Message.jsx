0bf3a3405ba99419ba61408e90c745a2926805f9
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-05-31 19:30:27 +0530

    messages workgin in real time

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import { useAuthContext  } from "../../../context/AuthContext"
+import useConversation from "../../zustand/useConversation";
+// import useSendMessage from "../../hooks/useSendMessage"
 
-const Message = () => {
+// now this takes message 
+const Message = ({message}) => {
+
+  const {authUser} = useAuthContext();
+  const { selectedConversation } = useConversation();
+  const fromMe = message.senderId === authUser._id;  //populated it in backend 
+
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? 'bg-blue-500' : "";
+
+
   return (
-    <div className='chat chat-end'>
+    // now after taking all dynamic values in variable above now we move to adding 
+    // these into outsystem (frontend)
+    <div className={`chat ${chatClassName}`}> 
         <div className='chat-image avatar'>
             <div className='w-10 rounded-full'>
-                <img src= "https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
-            alt="Tailwind CSS chat bubble component"/>
+                <img 
+                // src= "https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
+            alt="Tailwind CSS chat bubble component"
+            src={ profilePic }
+            />
 
             </div>
 
         </div>
-<div className='chat-bubble text-white bg-blue-500 '>
-    Hi! what is upp??
-
+<div className= {`chat-bubble text-white bg-blue-500  ${bubbleBgColor}`}>
+{message.message}
 </div>
 <div className='chat-footer opacity-50 text-xs flex gap-1 items-center '>
   12:42
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,6 +1,44 @@
 import React from 'react'
 import Message from './Message'
 import useGetMessages from '../../hooks/useGetMessages'
+import MessageSkeleton from '../skeletons/MessageSkeleton';
+
+const Messages = () => 
+  {
+
+  const {messages, loading} = useGetMessages();
+  console.log("messages: ", messages);
+  
+  return (
+    // this gives me loading screen skeleton of three conversations
+
+    <div className="px-4 flex-1 overflow-auto">
+
+      { !loading && messages.length > 0 && messages.map((message) => (
+        <Message key={message.id} message={message} />
+      ))}
+
+    
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)} 
+
+    { !loading && messages.length === 0 && (
+      <p className='text-center'>Send a message to start the conversation</p>
+    )}
+    </div>
+  );
+};
+
+export default Messages
+
+
+
+
+/*used in starting for sample messges
+
+
+import React from 'react'
+import Message from './Message'
+import useGetMessages from '../../hooks/useGetMessages'
 
 const Messages = () => {
 
@@ -26,4 +64,4 @@ const Messages = () => {
   )
 }
 
-export default Messages
+export default Messages*/
