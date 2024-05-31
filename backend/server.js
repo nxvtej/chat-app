8127fbe1ce01c619356aa29f4308d71623e57342
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-06-01 01:06:43 +0530

    deployment phase

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+import path from 'path';
 
 import authRoutes from"./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
@@ -12,6 +12,8 @@ import protectRoute from "./middleware/protectRoute.js";
 
 import { app, server } from './socket/socket.js';
 
+
+const __dirname = path.resolve(); //coming frmo node js
 // const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
@@ -22,6 +24,15 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages",protectRoute, messageRoutes); //important kind of auth
 app.use("/api/users", userRoutes);
+
+// middleware here
+// connected frontend to backend and both runnign at port 5000
+app.use(express.static(path.join(__dirname, '/frontend/dist'))); //for serving static files
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+})
+
 
 server.listen(PORT, ()=>{
     connectToMongoDB();
