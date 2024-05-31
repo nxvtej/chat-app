212ff3bf7e4b5ccfeb750dc3f28afd28fe6cb5ab
Navdeep <108007267+nxvtej@users.noreply.github.com>
2024-05-31 19:49:03 +0530

    fixed auto scroll using setTimeout

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+// import React from 'react'
 import Message from './Message'
 import useGetMessages from '../../hooks/useGetMessages'
 import MessageSkeleton from '../skeletons/MessageSkeleton';
+import { useEffect,  useRef } from 'react';
 
 const Messages = () => 
   {
@@ -9,13 +10,32 @@ const Messages = () =>
   const {messages, loading} = useGetMessages();
   console.log("messages: ", messages);
   
+
+  const lastMessageRef = useRef();
+
+  
+    // this is to auto scroll to the last message
+    // as rendnering takes some time direcly this wouldnt have wokred
+    // properly 
+    useEffect(() => {
+      setTimeout(() => {
+        lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+      }, 100);
+    }, [messages]);
+
   return (
     // this gives me loading screen skeleton of three conversations
 
     <div className="px-4 flex-1 overflow-auto">
 
-      { !loading && messages.length > 0 && messages.map((message) => (
-        <Message key={message.id} message={message} />
+      { !loading && 
+      messages.length > 0 && 
+      messages.map((message) => (
+        <div key={message._id}
+        ref = {lastMessageRef}
+        >
+          <Message  message={message} />
+        </div>
       ))}
 
     
